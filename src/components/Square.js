c3b8c7b68c9e6f8f0a1de39c5051a6a1e75f3fb5
AlejaH1213 <alejajara2@gmail.com>
2023-09-18 15:16:00 -0700

    click and an x appears

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
-const Square = (props) => {
+const Square = ({squares, handleClick}) => {
   return (
     <div className="grid">
-      {props.squares.map((squares,index) => (
-        <div key={index} className="square"></div>
+      {squares.map((squares,index) => (
+        <div 
+          key={index} 
+          className="square"
+          onClick={() => handleClick(index)}
+        >
+          {squares}
+        </div>
       ))}
     </div>
   )
